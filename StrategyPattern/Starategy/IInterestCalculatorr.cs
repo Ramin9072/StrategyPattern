5fb902b5cb542bb006191016e66b8f8093a64a12
r.navabian <r.navabian@NAVABIAN>
2023-03-26 11:09:29 +0330

    Add project files.

---
+++ StrategyPattern/Starategy/IInterestCalculatorr.cs
@@ -0,0 +1,24 @@
+﻿namespace StrategyPattern.Starategy
+{
+    public interface IInterestCalculator
+    {
+        double Calculate();
+    }
+
+    public class HighSessionIntresest : IInterestCalculator
+    {
+        public double Calculate()
+        {
+            return 0;
+        }
+    }
+
+
+    public class LowSessionIntresest : IInterestCalculator
+    {
+        public double Calculate()
+        {
+            return 0;
+        }
+    }
+}
